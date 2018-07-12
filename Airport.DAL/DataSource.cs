1591fc5cb40efb0fed456d39e103fd480254e9f2
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-07-12 12:12:43 +0300

    fixed

--- Airport.DAL/DataSource.cs
+++ Airport.DAL/DataSource.cs
@@ -7,13 +7,9 @@ namespace Airport.DAL
 {
     public class DataSource
     {
-        public List<Ticket> Tickets
-        {
-            get
-            {
-                return new List<Ticket> { new Ticket { Id = Guid.NewGuid(), Price = 49.90M } };
-            }
-        }
+        static List<Ticket> tickets = new List<Ticket> { new Ticket { Id = Guid.NewGuid(), Price = 49.90M } };
+
+        public List<Ticket> Tickets => tickets;
 
         public List<Aeroplane> Aeroplanes
         {
