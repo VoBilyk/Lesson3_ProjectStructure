671d0595c71f23465ecc3b8fdebe6fd016de2fac
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-07-13 00:24:53 +0300

    added bogus generator

--- Airport.DAL/DataSource.cs
+++ Airport.DAL/DataSource.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using Airport.DAL.Interfaces;
 using Airport.DAL.Models;
+using Bogus;
 
 namespace Airport.DAL
 {
     public class DataSource
     {
-        static List<Ticket> tickets = new List<Ticket> { new Ticket { Id = Guid.NewGuid(), Price = 49.90M } };
-        static List<Pilot> pilots = new List<Pilot> { new Pilot { Id = Guid.NewGuid(), FirstName = "Jonh"  } };
+        static List<Ticket> tickets;
+        static List<Pilot> pilots;
+        static List<Crew> crews;
+        static List<Stewardess> stewardesses;
+        static List<Aeroplane> aeroplanes;
+        static List<AeroplaneType> aeroplaneTypes;
+        static List<Flight> flights;
+        static List<Departure> departures;
+
+
+        static DataSource()
+        {
+            var pilotFaker = new Faker<Pilot>()
+                .RuleFor(o => o.Id, Guid.NewGuid())
+                .RuleFor(o => o.FirstName, f => f.Name.FirstName())
+                .RuleFor(o => o.SecondName, f => f.Name.LastName())
+                .RuleFor(o => o.Experience, new TimeSpan(Randomizer.Seed.Next(1000000)));
+
+            pilots = pilotFaker.Generate(10);
+
+
+            var ticketFaker = new Faker<Ticket>()
+                .RuleFor(o => o.Id, Guid.NewGuid())
+                .RuleFor(o => o.Price, f => f.Random.Number(20, 100));
+
+            tickets = ticketFaker.Generate(10);
+        }
 
 
 
@@ -19,7 +43,7 @@ namespace Airport.DAL
             {
                 return tickets as List<TEntity>;
             }
-            else if(pilots is List<TEntity>)
+            else if (pilots is List<TEntity>)
             {
                 return pilots as List<TEntity>;
             }
@@ -29,39 +53,18 @@ namespace Airport.DAL
 
         public List<Ticket> Tickets => tickets;
 
-        public List<Aeroplane> Aeroplanes
-        {
-            get;
-        }
+        public List<Aeroplane> Aeroplanes => aeroplanes;
 
-        public List<AeroplaneType> AeroplaneTypes
-        {
-            get;
-        }
+        public List<AeroplaneType> AeroplaneTypes => aeroplaneTypes;
 
-        public List<Crew> Crews
-        {
-            get;
-        }
+        public List<Crew> Crews => crews;
 
-        public List<Departure> Departures
-        {
-            get;
-        }
+        public List<Departure> Departures = departures;
 
-        public List<Flight> Flights
-        {
-            get;
-        }
+        public List<Flight> Flights => flights;
 
-        public List<Pilot> Pilots
-        {
-            get;
-        }
+        public List<Pilot> Pilots => pilots;
 
-        public List<Stewardess> Stewardesses
-        {
-            get;
-        }
+        public List<Stewardess> Stewardesses => stewardesses;
     }
 }
